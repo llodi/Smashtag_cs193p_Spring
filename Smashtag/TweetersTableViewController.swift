411a6d05f4998aa88e44cea9b4a29d69321530af
Ilya Dolgopolov <llodi@Mac-mini-Ilya.local>
2016-07-03 21:36:55 +0300

    lection 11 (preparing)

---
+++ Smashtag/TweetersTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TweetersTableViewController.swift
+//  Smashtag
+//
+//  Created by Ilya Dolgopolov on 03.07.16.
+//  Copyright © 2016 Ilya Dolgopolov. All rights reserved.
+//
+
+import UIKit
+
+class TweetersTableViewController: UITableViewController {
+
+     /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
